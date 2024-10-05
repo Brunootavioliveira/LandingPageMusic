5449b540124d86601de11c50852cbdf7985257a7
Bruno Oliveira <brunootavioliveira@gmail.com>
2024-10-04 21:25:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function playSound() {
+    var audio = document.getElementById('meuAudio');
+    audio.play();
     alert('Som ativado!');
+}
+
+function pauseSound() {
+    var audio = document.getElementById('meuAudio');
+    audio.pause();
+    alert('Som pausado!');
 }
